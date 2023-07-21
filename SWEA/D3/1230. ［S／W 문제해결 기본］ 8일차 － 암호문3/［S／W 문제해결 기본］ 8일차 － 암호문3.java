7d1a5d60a95057225874883f4cbd343f91a29a7b
Minji Ma <42285463+akalswl14@users.noreply.github.com>
2023-07-21 18:40:35 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 191 ms, Memory: 33,572 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
 class Solution {
 
   static final String INSERT = "I", DELETE = "D", ADD = "A";
-  static List<String> list;
+  static List<Integer> list;
   static int N, M, x, y;
 
   public static void main(String[] args) throws IOException {
@@ -29,7 +29,7 @@ class Solution {
 
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < N; j++) {
-        list.add(st.nextToken());
+        list.add(Integer.parseInt(st.nextToken()));
       }
 
       st = new StringTokenizer(br.readLine());
@@ -57,7 +57,7 @@ class Solution {
       x = Integer.parseInt(st.nextToken());
       y = Integer.parseInt(st.nextToken());
       while (y-- > 0) {
-        list.add(x++, st.nextToken());
+        list.add(x++, Integer.parseInt(st.nextToken()));
       }
     } else if (command.equals(DELETE)) {
       x = Integer.parseInt(st.nextToken());
@@ -68,7 +68,7 @@ class Solution {
     } else if (command.equals(ADD)) {
       y = Integer.parseInt(st.nextToken());
       while (y-- > 0) {
-        list.add(st.nextToken());
+        list.add(Integer.parseInt(st.nextToken()));
       }
     }
   }
